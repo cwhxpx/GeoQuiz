b88387a562ecfdf1fab252fb0fbf13071855549c
cwh <cwh@192-168-1-4.tpgi.com.au>
2015-09-29 15:17:27 +1000

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by cwh on 28/09/2015.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
